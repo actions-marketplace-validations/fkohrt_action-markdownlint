981196421b42f419e5a1d6d13a1c876f75fbc522
Florian Kohrt <fkohrt@anche.no>
2020-06-21 17:21:07 +0200

    add pipefail

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ cd "${GITHUB_WORKSPACE}" || exit
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 if [ "${INPUT_REPORTER}" = x"github-pr-review" ]; then
-  markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%s:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
+  (set -o pipefail && markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
+    reviewdog -efm="%s:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}")
 else
-  markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%f:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
+  (set -o pipefail && markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
+    reviewdog -efm="%f:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}")
 fi
