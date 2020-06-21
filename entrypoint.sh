e58fab1235740d1dcc96f1e9a590f4bfa4cc7510
Florian Kohrt <fkohrt@anche.no>
2020-06-21 16:18:49 +0200

    Add filter-mode and fail-on-error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 if [ "${INPUT_REPORTER}" = x"github-pr-review" ]; then
   markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%s:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}" -filter-mode=nofilter
+    reviewdog -efm="%s:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
 else
   markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%f:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}" -filter-mode=nofilter
+    reviewdog -efm="%f:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
 fi
